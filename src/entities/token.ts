cb101e5571278c213781d4d917cb1810723954b1
cherideal <101du@163.com>
2021-04-25 17:10:22 +0800

    support oasis-eth network

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -79,5 +79,6 @@ export const WETH9: { [chainId in ChainId]: Token } = {
     'Wrapped Ether'
   ),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH9', 'Wrapped Ether'),
-  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH9', 'Wrapped Ether')
+  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH9', 'Wrapped Ether'),
+  [ChainId.OETH]: new Token(ChainId.OETH, '0xF6A936063AE8A0b7c193c685b992fA29E69828b1', 18, 'WOETH', 'Wrapped Oasis Ether')
 }
